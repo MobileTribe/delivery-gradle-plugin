d4488ad1d6621c0d0d0fe4a83e32f948140d41d5
Alexandre Lecerf <alexandre.lecerf@leroymerlin.fr>
2017-02-27 09:53:48 +0100

    Add -c param

--- delivery-plugin/src/main/groovy/com/leroymerlin/plugins/core/GitHandler.groovy
+++ delivery-plugin/src/main/groovy/com/leroymerlin/plugins/core/GitHandler.groovy
@@ -10,73 +10,66 @@ import org.gradle.api.Project
  */
 class GitHandler extends Executor implements BaseScmAdapter {
 
-    URI baseUri
+    String email, username
 
     @Override
     void setup(Project project, DeliveryPluginExtension extension) {
         if (!"git --version".execute().text.contains('git version')) {
             throw new GradleException("Git not found, install Git before continue")
         } else {
-            String password = System.getProperty('SCM_PASSWORD')
-            String user = System.getProperty('SCM_USER')
-            if (password == null || user == null) {
+            email = System.getProperty('SCM_EMAIL')
+            username = System.getProperty('SCM_USER')
+            if (email == null || username == null) {
                 throw new GradleException('Add credentials to continue')
             }
 
             if (!new File('.git').exists()) {
                 println(exec(['git', 'init'], directory: 'delivery-test', errorMessage: "Failed to init Git", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
-            } else {
-                baseUri = new URI(exec(['git', 'config', '--local', '--get', 'remote.origin.url'], errorMessage: "Fail to read origin url").readLines()[0])
-                String domain = baseUri.getHost() + baseUri.path
-                String credential = "${baseUri.getScheme()}://$user:$password@$domain"
-                exec(['git', 'remote', 'rm', 'origin'], errorMessage: "Fail to remove origin")
-                exec(['git', 'remote', 'add', 'origin', credential], errorMessage: "Fail to add origin")
             }
         }
     }
 
     @Override
     void release() {
-        System.clearProperty('SCM_PASSWORD')
+        System.clearProperty('SCM_EMAIL')
         System.clearProperty('SCM_USER')
-        exec(['git', 'remote', 'rm', 'origin'], errorMessage: "Fail to remove origin")
     }
 
     @Override
     String addAllFiles() {
-        return println(exec(['git', 'add', '.'], directory: 'delivery-test', errorMessage: "Failed to add files", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'add', '.'], directory: 'delivery-test', errorMessage: "Failed to add files", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
     }
 
     @Override
     String commit(String comment) {
-        return println(exec(['git', 'commit', '-am', "\'" + comment + "\'"], directory: 'delivery-test', errorMessage: "Failed to commit", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'commit', '-am', "\'" + comment + "\'"], directory: 'delivery-test', errorMessage: "Failed to commit", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
     }
 
     @Override
     String deleteBranch(String branchName) {
-        return println(exec(['git', 'branch', '-d', branchName], directory: 'delivery-test', errorMessage: "Failed to delete $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'branch', '-d', branchName], directory: 'delivery-test', errorMessage: "Failed to delete $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
     }
 
     @Override
     String switchBranch(String branchName, boolean createIfNeeded) {
         if (createIfNeeded)
-            return println(exec(['git', 'checkout', '-B', branchName], directory: 'delivery-test', errorMessage: "Couldn't create $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+            return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'checkout', '-B', branchName], directory: 'delivery-test', errorMessage: "Couldn't create $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
         else
-            return println(exec(['git', 'checkout', branchName], directory: 'delivery-test', errorMessage: "Failed to switch to $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+            return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'checkout', branchName], directory: 'delivery-test', errorMessage: "Failed to switch to $branchName", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
     }
 
     @Override
     String tag(String annotation, String message) {
-        return println(exec(['git', 'tag', '-a', annotation, '-m', '\'' + message + '\''], directory: 'delivery-test', errorMessage: "Duplicate tag [$annotation]", errorPatterns: ['already exists']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'tag', '-a', annotation, '-m', '\'' + message + '\''], directory: 'delivery-test', errorMessage: "Duplicate tag [$annotation]", errorPatterns: ['already exists']))
     }
 
     @Override
     String merge(String from) {
-        return println(exec(['git', 'merge', '--no-ff', from], directory: 'delivery-test', errorMessage: "Failed to merge $from", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'merge', '--no-ff', from], directory: 'delivery-test', errorMessage: "Failed to merge $from", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
     }
 
     @Override
     String push() {
-        return println(exec(['git', 'push'], directory: 'delivery-test', errorMessage: 'Failed to push to remote', errorPatterns: ['[rejected]', 'error: ', 'fatal: ']))
+        return println(exec(['git', '-c', "user.name=$username", '-c', "user.email=$email", 'push'], directory: ' delivery - test ', errorMessage: ' Failed to push to remote ', errorPatterns: ['[rejected] ', ' error: ', ' fatal: ']))
     }
 }
