10470407491651be3d6d955eb20129504ac621bf
Alexandre Lecerf <alexandre.lecerf@leroymerlin.fr>
2017-03-20 14:27:59 +0100

    Add Android library test

---
+++ delivery-plugin/src/test/groovy/com/leroymerlin/plugins/test/integration/AndroidLibraryTest.groovy
@@ -0,0 +1,42 @@
+package com.leroymerlin.plugins.test.integration
+
+import groovy.io.FileType
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by alexandre on 17/12/15.
+ */
+class AndroidLibraryTest extends AbstractIntegrationTest {
+
+    @Override
+    String getProjectName() {
+        return "androidLibrary"
+    }
+
+    @Test
+    void testBuildTaskGeneration() {
+        def archiveDirectory = new File(workingDirectory, "build/archive")
+        applyExtraGradle('''
+delivery{
+    archiveRepositories = {
+        maven {
+            url uri("''' + archiveDirectory.absolutePath + '''")
+        }
+    }
+    flows{
+        build{
+            build
+        }
+    }
+}
+''')
+        testTask('buildFlow')
+        def list = []
+        archiveDirectory.eachFileRecurse(FileType.FILES, {
+            f ->
+                list << f
+        })
+        Assert.assertEquals("archive folder should contain 8 files", 8, list.size());
+    }
+}
