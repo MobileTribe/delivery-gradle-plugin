19b4dba3aa545eb92f34871ca4ea38d621d4e769
Alexandre Lecerf <contact.alecerf@gmail.com>
2017-04-13 14:50:33 +0200

    feat (Executor) : add log levels

--- delivery-plugin/src/main/groovy/com/leroymerlin/plugins/cli/Executor.groovy
+++ delivery-plugin/src/main/groovy/com/leroymerlin/plugins/cli/Executor.groovy
@@ -1,6 +1,7 @@
 package com.leroymerlin.plugins.cli
 
 import org.gradle.api.GradleException
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.logging.Logger
 
 /**
@@ -10,45 +11,30 @@ import org.gradle.api.logging.Logger
 class Executor {
 
     public static Logger logger
+    public static Map optionsMap
+    public static List<String> commandsList
+    public static LogLevel level
 
     static String exec(
             Map options = [:],
             List<String> commands
     ) {
+        optionsMap = options
+        commandsList = commands
+
         StringBuffer out = new StringBuffer()
-        StringBuffer err = new StringBuffer()
 
         File directory = options['directory'] ? options['directory'] as File : null
+        level = options['logLevel'] ? options['logLevel'] as LogLevel : null
         List processEnv = options['env'] ? ((options['env'] as Map) << System.getenv()).collect {
             "$it.key=$it.value"
         } : null
-
-        logger?.info("Running $commands in [$directory]")
+        if (level != null)
+            logger?.log(level, "Running $commands in [$directory]")
         Process process = commands.execute(processEnv, directory)
-        logger?.info("Running $commands produced output: [${out.toString().trim()}]")
+        waitForProcessOutput(process, out)
 
-        process.waitForProcessOutput(out, err)
-
-        if (err.toString()) {
-            def message = "Running $commands produced an error: [${err.toString().trim()}]"
-
-            if (options['failOnStderr'] as boolean) {
-                throw new GradleException(message)
-            } else {
-                logger?.warn(message)
-            }
-        }
-
-        if (options['errorPatterns'] && [out, err]*.toString().any { String s ->
-            (options['errorPatterns'] as List<String>).any {
-                s.contains(it)
-            }
-        }) {
-            throw new GradleException("${options['errorMessage'] ? options['errorMessage'] as String : 'Failed to run [' + commands.join(' ') + ']'} - [$out][$err]")
-        }
-
-        def result = out.toString()
-        return result
+        return out.toString()
     }
 
     static List<String> convertToCommandLine(String cmd) {
@@ -57,5 +43,66 @@ class Executor {
         for (int i = 0; st.hasMoreTokens(); i++)
             cmdList.add(st.nextToken())
         cmdList
+    }
+
+    static void waitForProcessOutput(Process process, Appendable output) {
+        Thread tout = new Thread(new TextDumper(process.getInputStream(), false, output))
+        Thread terr = new Thread(new TextDumper(process.getErrorStream(), true, output))
+
+        tout.start()
+        terr.start()
+        tout.join()
+        terr.join()
+
+        process.waitFor()
+        process.closeStreams()
+    }
+
+    private static class TextDumper implements Runnable {
+        InputStream input
+        boolean catchError
+        Appendable app
+
+        TextDumper(InputStream inputStream, boolean catchError, Appendable app) {
+            this.input = inputStream
+            this.catchError = catchError
+            this.app = app
+        }
+
+        void run() {
+            InputStreamReader isr = new InputStreamReader(this.input)
+            BufferedReader br = new BufferedReader(isr)
+
+            try {
+                String next
+                while ((next = br.readLine()) != null) {
+                    if (this.app != null) {
+                        this.app.append(next)
+                        this.app.append("\n")
+                    }
+                    if (catchError) {
+                        if (optionsMap['failOnStderr'] as boolean) {
+                            throw new GradleException("Running $commandsList produced an error: ${next}")
+                        } else {
+                            if (level != null)
+                                logger?.log(level, next)
+                        }
+                    } else {
+                        if (level != null)
+                            logger?.log(level, next)
+                    }
+
+                    if (optionsMap['errorPatterns'] && [next]*.toString().any { String s ->
+                        (optionsMap['errorPatterns'] as List<String>).any {
+                            s.contains(it)
+                        }
+                    }) {
+                        throw new GradleException("${optionsMap['errorMessage'] ? optionsMap['errorMessage'] as String : 'Failed to run [' + commandsList.join(' ') + ']'} - [$next]")
+                    }
+                }
+            } catch (IOException var5) {
+                throw new GroovyRuntimeException("exception while reading process stream", var5)
+            }
+        }
     }
 }
