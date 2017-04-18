824f0a4c0536020905ea7de704e3ae651cea96cd
Alexandre Lecerf <contact.alecerf@gmail.com>
2017-04-18 09:36:13 +0200

    feat (DeliveryBuild) : use getExtension method

--- delivery-plugin/src/main/groovy/com/leroymerlin/plugins/tasks/build/DeliveryBuild.groovy
+++ delivery-plugin/src/main/groovy/com/leroymerlin/plugins/tasks/build/DeliveryBuild.groovy
@@ -2,7 +2,7 @@ package com.leroymerlin.plugins.tasks.build
 
 import com.leroymerlin.plugins.cli.Executor
 import com.leroymerlin.plugins.entities.ArchiveArtifact
-import com.sun.javafx.logging.Logger
+import org.apache.commons.io.FilenameUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.PublishArtifact
 import org.gradle.api.tasks.Input
@@ -27,15 +27,10 @@ class DeliveryBuild extends DefaultTask {
             variantName = project.projectName
         }
         return outputFiles.collect { classifier, file ->
-            String extension = "";
-            int i = file.name.lastIndexOf('.')
-            if (i > 0) {
-                extension = file.name.substring(i + 1)
-            }
+            String extension = FilenameUtils.getExtension(file.name)
             return new ArchiveArtifact(variantName, extension, classifier, file, this)
         }
     }
-
 
     def cmd(String cmd) {
         return Executor.exec(Executor.convertToCommandLine(cmd), directory: project.projectDir)
