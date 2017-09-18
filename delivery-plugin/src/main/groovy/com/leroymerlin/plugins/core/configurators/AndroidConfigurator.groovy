381c82d464ffafe6db173d001bbfb10523b68be1
paillardf <paillard.f@gmail.com>
2017-09-18 17:08:27 +0200

    chore (version) : update version to 3.0.0-BETA12

--- delivery-plugin/src/main/groovy/com/leroymerlin/plugins/core/configurators/AndroidConfigurator.groovy
+++ delivery-plugin/src/main/groovy/com/leroymerlin/plugins/core/configurators/AndroidConfigurator.groovy
@@ -88,10 +88,14 @@ class AndroidConfigurator extends ProjectConfigurator {
         } else {
             project.android.libraryVariants.all { currentVariant ->
                 if (currentVariant.buildType.name == "release") {
-                    def buildTaskName = "build${project.projectName.capitalize()}Artifacts"
+                    String flavorName = project.projectName.toString().split(' ').collect({ m -> return m.toLowerCase().capitalize() }).join("") + (currentVariant.flavorName.capitalize() ? "-${currentVariant.flavorName.capitalize()}" : "")
+                    flavorName = flavorName[0].toLowerCase() + flavorName.substring(1)
+                    String flavorNameNexus = project.projectName.toString().split(' ').collect({ m -> return m.toLowerCase() }).join("-") + (currentVariant.flavorName.toLowerCase() ? "-${currentVariant.flavorName.toLowerCase()}" : "")
+
+                    def buildTaskName = "build${flavorName.capitalize()}Artifacts"
                     if (project.tasks.findByPath(buildTaskName) == null) {
                         project.task(buildTaskName, type: AndroidLibBuild, group: DeliveryPlugin.TASK_GROUP) {
-                            variantName project.projectName
+                            variantName flavorNameNexus
                         }
                     }
                     project.tasks.findByPath(buildTaskName).addVariant(currentVariant)
