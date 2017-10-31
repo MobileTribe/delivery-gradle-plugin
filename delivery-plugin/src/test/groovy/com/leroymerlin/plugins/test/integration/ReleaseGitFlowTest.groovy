266b4c367374acc75d25b0e686a6c266832af90a
ALEXANDRE LECERF <alexandre.lecerf@leroymerlin.fr>
2017-10-31 13:32:51 +0100

    fix (releaseGitFLowTest) : remove git repo

--- delivery-plugin/src/test/groovy/com/leroymerlin/plugins/test/integration/ReleaseGitFlowTest.groovy
+++ delivery-plugin/src/test/groovy/com/leroymerlin/plugins/test/integration/ReleaseGitFlowTest.groovy
@@ -1,5 +1,6 @@
 package com.leroymerlin.plugins.test.integration
 
+import com.leroymerlin.plugins.cli.Executor
 import org.junit.Test
 
 /**
@@ -15,6 +16,15 @@ class ReleaseGitFlowTest extends AbstractIntegrationTest {
     @Test
     void releaseGitFLowTest() {
         def archiveDirectory = new File(workingDirectory, "build/archive")
+        Executor.exec(['git', 'init'], [directory: project.rootDir, errorMessage: "Failed to init git", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'remote', 'add', 'origin', '.'], [directory: project.rootDir, errorMessage: "Failed to add origin", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'add', '.'], [directory: project.rootDir, errorMessage: "Failed to add", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'commit', '-am', 'first commit'], [directory: project.rootDir, errorMessage: "Failed to commit", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'push', '--set-upstream', 'origin', 'master'], [directory: project.rootDir, errorMessage: "Failed to push", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'checkout', '-B', 'develop'], [directory: project.rootDir, errorMessage: "Failed to create branch", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'add', '.'], [directory: project.rootDir, errorMessage: "Failed to add files", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'commit', '-am', 'first commit'], [directory: project.rootDir, errorMessage: "Failed to commit files", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
+        Executor.exec(['git', 'push', '--set-upstream', 'origin', 'develop'], [directory: project.rootDir, errorMessage: "Failed to push", errorPatterns: ['[rejected]', 'error: ', 'fatal: ']])
         applyExtraGradle('''
 delivery{
 
