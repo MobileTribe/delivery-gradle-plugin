da6b6754f00de6bb1ce29bd2924f03f927adc1b2
paillardf <paillard.f@gmail.com>
2018-09-25 11:50:08 +0200

    refactor(Executor): refactor executor to handle error

---
+++ delivery-plugin/src/test/groovy/com/leroymerlin/plugins/test/ExecutorTest.groovy
@@ -0,0 +1,26 @@
+package com.leroymerlin.plugins.test
+
+import com.leroymerlin.plugins.cli.Executor
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ExecutorTest {
+
+    @Before
+    void setUp() {
+    }
+
+    @After
+    void tearDown() {
+    }
+
+    @Test
+    void testDeliveryExtension() {
+        def result = Executor.exec(["unknownCommand"]) {
+            needSuccessExitCode = false
+            directory = new File("")
+        }
+        assert result.exitValue == 127
+    }
+}
